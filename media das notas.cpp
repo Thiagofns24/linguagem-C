15532e3c8f938ac440b4a6576e2dcf940a7911a8
Thiagofns24 <80801496+Thiagofns24@users.noreply.github.com>
2021-04-05 17:43:18 -0300

    Add files via upload

---
+++ media das notas.cpp
@@ -0,0 +1,29 @@
+//Objetivo: Calcular a média de duas notas
+//Entrada: Nota1, Nota2
+//Saída: Média
+
+#include<stdio.h>
+#include<locale.h>
+int main(){
+	
+// Declaração de variavéis
+int  Nota1, Nota2, Media;
+
+// Leitura de variavéis
+printf("Coloque aqui sua primeira nota:\n");
+scanf("%f", &Nota1);
+printf("Coloque aqui sua segunda nota:\n");
+scanf("%f", &Nota2);
+
+// Processamento dos dados
+Media = Nota1 + Nota2 /2;
+
+// Resultado
+setlocale(LC_ALL, "");
+printf("O valor da sua média, é:\n", Media);
+return 0;
+}
+
+
+
+
